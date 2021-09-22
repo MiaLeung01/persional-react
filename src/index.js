8930d8fb74a7ecaae2a6cecc4a44996da7a0b3ca
liangshuwen <liangshuwen@puyiwm.com>
2021-09-22 19:27:54 +0800

    功能：提交

--- src/index.js
+++ src/index.js
@@ -24,18 +24,50 @@ class Btn extends React.Component {
   }
   componentDidMount () {
     console.log('btn Did mount')
+    setTimeout(() => {
+      console.log(this.state.num)
+        this.setState({
+          num: this.state.num + 1
+        })
+    }, 1000)
   }
   render() {
     // return React.createElement(Child, {name: 'mia', data: this.state.num})
-    const self = this
-    return  React.createElement('button', {
-      onClick: function() {
-        console.log('button click');
-        self.setState({
-          num: 2
-        })
-      }
-    }, this.state.num)
+    // const self = this
+    // return  React.createElement('button', {
+    //   onClick: function() {
+    //     console.log('button click');
+    //     self.setState({
+    //       num: 2
+    //     })
+    //   }
+    // }, this.state.num)
+    // return React.createElement('div', {
+    //   onClick: function() {
+    //     console.log('button click')
+    //   },
+    //   style: {
+    //     color: 'white',
+    //     backgroundColor: this.state.num % 2 === 0 ? 'green' : 'pink',
+    //     fontSize: '20px'
+    //   },
+    //   className: 'new old'
+    // }, this.state.num)
+    return this.state.num %  2 === 1 ? 
+      React.createElement('ul', {}, 
+        React.createElement('li', {}, 'A'),
+        React.createElement('li', {}, 'B'),
+        React.createElement('li', {}, 'C'),
+        React.createElement('li', {}, 'D'),
+      )
+       :
+      React.createElement('ul', {}, 
+        React.createElement('li', {}, 'A'),
+        React.createElement('li', {}, 'C'),
+        React.createElement('li', {}, 'B'),
+        React.createElement('li', {}, 'E'),
+        React.createElement('li', {}, 'F'),
+      )
   }
 }
  
--- src/lib/react.js
+++ src/lib/react.js
@@ -19,15 +19,136 @@ class ReactUnit {
   }
 }
 class ReactTextUnit extends ReactUnit {
+  update(newEle) {
+    if (this._currentElement !== newEle) {
+      $(`[data-reactid=${this._rootId}]`).html(newEle)
+    }
+  }
   getMarkup(id) {
-    this._reactId = id
+    this._rootId = id
     const markUp = `<sapn data-reactid="${id}">${this._currentElement}</sapn>`
     return markUp
   }
 }
 class ReactNativeUnit extends ReactUnit {
+  update(newEle) {
+    let newProps = newEle.props
+    let oldProps = this._currentElement.props
+    this.updateDomProperties(oldProps, newProps)
+    this.updateDomChildren(newEle.props.children)
+  }
+  updateDomProperties(oldProps, newProps) {
+    for (let k in oldProps) {
+      // 删除属性
+      if (!newProps.hasOwnProperty(k)) {
+        $(`[data-reactid=${this._rootId}]`).removeProp(k)
+      }
+      // 删除事件
+      if(k.startsWith('on')) {
+        $(`[data-reactid=${this._rootId}]`).undelegate()
+      }
+    }
+    for (let key in newProps) {
+      if (key.startsWith('on')) {
+        const action = key.slice(2).toLowerCase()
+        $(`[data-reactid=${this._rootId}]`).delegate(`[data-reactid=${this._rootId}]`, `${action}.${this._rootId}}`, newProps[key])
+        continue
+      }
+      if (key === 'className') {
+        $(`[data-reactid=${this._rootId}]`).attr('class', newProps[key])
+        continue
+      }
+      if (key === 'style') {
+        Object.entries(newProps.style).map(([k, v]) => {
+          k = k.replace(/([A-Z])/g, (match) => {
+            return '-' + match.toLowerCase()
+          })
+          $(`[data-reactid=${this._rootId}]`).css(k, v)
+        })
+        continue
+      }
+      if(key === 'children') {
+        continue
+      }
+      $(`[data-reactid=${this._rootId}]`).prop(key, newProps[key])
+    }  
+  }
+  updateDomChildren(newChildrenElement) {
+    const oldChildrenUnitMap = this.getOldChildrenMap()
+    const newChildrenUnit = this.getNewChildren(oldChildrenUnitMap, newChildrenElement)
+    this.diff(newChildrenUnit)
+  }
+  getOldChildrenMap() {
+    const children = this._currentChildrenUnit
+    const childMap = new Map()
+    children.forEach((childUnit) => {
+      const key =  childUnit._currentElement?.props?.key || childUnit.index
+      childMap.set(key, childUnit)
+    })
+    console.log(childMap)
+    return childMap 
+  }
+  getNewChildren( oldChildrenUnitMap, newChildElements) {
+    console.log('this._currentChildrenUnit', this._currentChildrenUnit)
+    const newChildrenUnit = []
+    newChildElements.forEach((newChildElement, i) => {
+      const newChildKey = newChildElement.props?.key || i
+      const oldChildUnit = oldChildrenUnitMap.get(newChildKey)
+      if( shouldDeepCompare(oldChildUnit, newChildKey) ) {
+        oldChildUnit.update(newChildElement)
+        newChildrenUnit(oldChildUnit)
+        this._currentChildrenUnit[i] = oldChildUnit
+      } else {
+        const newChildUnit = createReactUnit(newChildElement)
+        newChildrenUnit.push(newChildUnit)
+        this._currentChildrenUnit[i] = newChildUnit
+      }
+    })
+    console.log('this._currentChildrenUnit new', this._currentChildrenUnit)
+    console.log('newChildrenUnit', newChildrenUnit)
+    return newChildrenUnit
+  }
+  diff (newChildrenUnit) {
+    let diffQueue = []
+    let lastIndex = 0
+    const oldChildMap = this.getOldChildrenMap()
+    console.log('oldChildMap', oldChildMap)
+    newChildrenUnit.forEach((newChild, i) => {
+      const newChildKey = newChild.props?.key || i
+      const oldChild = oldChildMap.get(newChildKey)
+      // 没有 oldChild, 新增
+      if (!oldChild) {
+        diffQueue.push({
+          action: 'Insert',
+          index: i,
+          element: newChild
+        })
+        lastIndex = i
+      } else {
+        const oldChildKey =  oldChild._currentElement?.props?.key || oldChild.index
+        if (oldChild._currentElement.type !== newChild._currentElement.type) {
+          diffQueue.push({
+            action: 'Remove',
+            index: i
+          })
+        } else {
+          if (oldChildKey < lastIndex) {
+            // 移动
+            diffQueue.push({
+              action: 'Move',
+              fromIndex: oldChildKey,
+              toIndex: lastIndex,
+              element: newChild
+            })
+            lastIndex += 1
+          }
+        }
+      }
+    })
+    console.log('diffQueue', diffQueue)
+  }
   getMarkup(id) {
-    this._reactId = id
+    this._rootId = id
     const {type: tag, props} = this._currentElement
     let tagStart = `<${tag} data-reactid="${id}" `
     let content = ''
@@ -35,52 +156,97 @@ class ReactNativeUnit extends ReactUnit {
     for (let key in props) {
       if (key.startsWith('on')) {
         const action = key.slice(2).toLowerCase()
-        $(document).delegate(`[data-reactid=${id}]`, `${action}.${this._reactId}}`, props[key])
+        $(document).delegate(`[data-reactid=${id}]`, `${action}.${this._rootId}}`, props[key])
+        continue
       }
       if (key === 'className') {
         tagStart += ` ${key}="${props[key]}"`
+        continue
       }
       if (key === 'style') {
         const styleObj = props.style
-        tagStart += Object.entries(styleObj).map(([k, v]) => {
+        const styleVal = Object.entries(styleObj).map(([k, v]) => {
           k = k.replace(/([A-Z])/g, (match) => {
             return '-' + match.toLowerCase()
           })
           return `${k}: ${v};`
         }).join(' ')
+        tagStart = `${tagStart} style="${styleVal}"`
+        continue
       }
       if (key === 'children') {
+        let children = []
         content = props.children.map((el, i) => {
-          return createReactUnit(el).getMarkup(`${id}.${i}`)
+          const childUnit = createReactUnit(el)
+          childUnit.index = i
+          children.push(childUnit)
+          return childUnit.getMarkup(`${id}.${i}`)
         }).join('')
+        this._currentChildrenUnit = children
+        continue
       }
       tagStart += ` ${key}="${props[key]}"`
     }
-    return tagStart + '>' + content + tagEnd
-  }
-}
-class ReactCompositeUnit extends ReactUnit {
-  getMarkup(id) {
-    this._reactId = id
-    const {type: Component, props} = this._currentElement
-    const instance = new Component(props)
-    instance.componentWillMount()
-    const unit = createReactUnit(instance.render())
-    const markUp = unit.getMarkup(id)
-    instance.componentDidMount()
+    const markUp = tagStart + '>' + content + tagEnd
     return markUp
   }
 }
+class ReactCompositeUnit extends ReactUnit {
+  update(newEle, newState) {
+    this._currentElement = newEle ? newEle : this._currentElement
+    let nextState = Object.assign(this._currentInstance.state, newState)
+    let nextProps = this._currentElement.props
+    // 下一级元素
+    const preRenderedElement = this._currentUnit._currentElement
+    // 新的下一级元素
+    const newRenderedElement = this._currentInstance.render()
+    if (!this._currentInstance?.shouldComponentUpdate?.(nextState, nextProps)) {
+      // 不更新
+      return
+    }
+    if (shouldDeepCompare(preRenderedElement, newRenderedElement)) {
+      this._currentUnit.update(newRenderedElement)
+      // didUpdate
+      this._currentInstance.componentDidUpdate?.()
+    } else {
+      // 交给下一级全部更新
+      const unit = createReactUnit(newRenderedElement)
+      this._currentUnit = unit
+      const markUp = unit.getMarkup(this._rootId)
+      $(`[data-reactid=${this._rootId}]`).replaceWith(markUp)
+    }
+  }
+  getMarkup(id) {
+    this._rootId = id
+    const {type: Component, props} = this._currentElement
+    this._currentInstance = new Component(props)
+    this._currentInstance.componentWillMount()
+    this._currentUnit = createReactUnit(this._currentInstance.render())
+    // 把实例 Unit 挂到实例，让它能访问到 Unit.update
+    this._currentInstance._currentUnit = this
+    const markUp = this._currentUnit.getMarkup(id)
+    this._currentInstance.componentDidMount()
+    return markUp
+  }
+}
+function shouldDeepCompare(oldEle, newEle) {
+  if (typeof newEle !== typeof oldEle) {
+    return false
+  }
+  if (newEle instanceof Element && oldEle instanceof Element ) {
+    return newEle.type === oldEle.type
+  }
+}
 
-function createReactUnit (elemnent) {
-  if (typeof elemnent === 'string' || typeof elemnent === 'number') {
-    return new ReactTextUnit(elemnent)
+function createReactUnit (element) {
+  if (typeof element === 'string' || typeof element === 'number') {
+    return new ReactTextUnit(element)
   }
-  if (typeof elemnent === 'object' && typeof elemnent.type === 'string') {
-    return new ReactNativeUnit(elemnent)
+  if (element instanceof Element && typeof element.type === 'string') {
+    return new ReactNativeUnit(element)
   }
-  if (typeof elemnent === 'object' && typeof elemnent.type === 'function') {
-    return new ReactCompositeUnit(elemnent)
+  if (element instanceof Element && typeof element.type === 'function') {
+    return new ReactCompositeUnit(element)
   }
 }
 
@@ -102,12 +268,12 @@ function createElement(type, props, ...children) {
 class Component {
   constructor(props) {
     this.props = props
-    this.state = {}
+  }
+  shouldComponentUpdate (nextState, nextProps) {
+    return true
   }
   setState(newState) {
-    Object.assign(this.state, newState)
-    console.log('setState', this.state, newState)
-    this.render()
+    this._currentUnit.update(null, newState)
   }
 } 
 
